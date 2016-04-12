9d72cb7afb5b9a181d97d85a56543551bf01f68a
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-04-11 19:15:37 -0500

    blog is working

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,27 +1,22 @@
-$(document).ready(function() {
-
-Blog();
-
-});
-
-var Blog = function() {
-
-$.getJSON('assets/js/blogging.json',function(data){
-	$(data.entries).each(function(index,value){
-		var p = '<small class="date">' + value.date + '</small>';
-		p += '<h3 class="blog-title">' + value.title + '</h3>';
-		p += '<div class="blog-div">' + '<p class="blogPosts">' + value.text + '</p>' + '</div>' + '<hr class ="blog-hr">';
-
-		posts.push(p);
-		console.log(p);
-		document.write(p);
+function request() {
+      return $.ajax({
+        type : 'GET',
+        dataType : 'json',
+        cache : 'false',
+        url : "blogging.json",
 
 
-	});
-
-
-});
-
-
-
-}
+        success : function(r) {
+          $('#blog').append('<div class="row">');
+          $.each(r.posts, function(key, value){
+            console.log(value.titleName);
+            console.log(value.date);
+            console.log(value.blogPost)
+            $('#blog').append('<div class="col-lg-12"><p class="titleName">' + value.titleName + '</p><p class="date">' + value.date + '</p><p class="blogPost">' + value.blogPost + '</p><hr class="featurette-divider"></div>');
+          });
+          $('#blog').append('</div>');
+        }
+      });
+    }
+   
+    request();
