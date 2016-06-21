9a05f403ab704e884ecc889ba726cae84ab0aa8b
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-06-21 14:57:25 -0500

    update

--- portfolio/app.js
+++ portfolio/app.js
@@ -7,14 +7,14 @@ function request() {
 
 
         success : function(r) {
-          $('#blog').append('<div class="row">');
+          
           $.each(r.posts, function(key, value){
             console.log(value.titleName);
             console.log(value.date);
             console.log(value.blogPost)
-            $('#blog').append('<div class="col-md-12"><table id="blogSize"><tr><th><p class="titleName">' + value.titleName + '</p></th></tr><tr><td><p class="date">' + value.date + '</p><p class="blogPost">' + value.blogPost + '</p></td></tr></div></table><br>');
+            $('#blog').append('<table id="blogSize"><tr><th><p class="titleName">' + value.titleName + '</p></th></tr><tr><td><p class="date">' + value.date + '</p><p class="blogPost">' + value.blogPost + '</p></td></tr></table><br>');
           });
-          $('#blog').append('</div>');
+      
         }
       });
     }
